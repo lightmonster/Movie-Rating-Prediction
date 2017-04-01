a149fa1dd2b2e2e48f12e3960bcd6d0eff5dc8a2
Suyie <qilezhi2@illinois.edu>
2017-03-31 22:42:05 -0500

    看上去可以了

--- final.py
+++ final.py
@@ -27,21 +27,36 @@ model = []
 # Object: User
 
 class User:
+    gender_total = 0
+    age_total = 0
+    occupation_total = 0
+    gender_number = 0
+    age_number = 0
+    occupation_number = 0
+
     def __init__(self,ID,gender,age,occupation):
         self.ID = ID
         if gender != "N/A":
             if gender == 'M':
                 self.gender = 0
+                User.gender_total += 0
+                User.gender_number += 1
             else:
                 self.gender = 1
+                User.gender_total += 1
+                User.gender_number += 1
         else:
             self.gender = None
         if age != "N/A":
             self.age = age
+            User.age_total += int(age)
+            User.age_number += 1
         else:
             self.age = None
         if occupation != "N/A":
             self.occupation = occupation
+            User.occupation_number += 1
+            User.occupation_total += int(occupation)
         else:
             self.occupation = None
 
@@ -52,11 +67,6 @@ class Movie:
         self.year = year
         self.genre = genre
 
-# Functions defined here
-
-def min_max (min_a, max_a, v):
-    return 7*(v-min_a)/(max_a-min_a)
-
 # Debug
 def print_dict_u (d):
     key_list = sorted(d.keys())
@@ -66,21 +76,6 @@ def print_dict_m (d):
     key_list = sorted(d.keys())
     for k in key_list:
         print k, d[k].year, d[k].genre
-
-def regression (X, y): # return b_list, mean of err_list
-    n = len(X)
-    # (1) X'y Matrix
-    Xt = np.transpose (X)
-    X_y = np.dot (Xt, y)
-    # (2) X'X Matrix
-    Xt_X = np.dot (Xt, X)
-    # (6) Inverse Matrix
-    inv_Xt_X = np.linalg.inv(Xt_X)
-    b_list = np.dot(inv_Xt_X, X_y)
-    err_list = []
-    for i in range(n):
-        err_list.append(y[i] - np.dot (b_list,X[i]))
-    return [b_list, np.mean(err_list)]
 
 # Extract Data: User
 
@@ -151,6 +146,9 @@ for i in range(num_genre+1):
 with open("train.txt") as f:
     data = f.readlines()
 
+def min_max (min_a, max_a, v):
+    return 7*(v-min_a)/(max_a-min_a)
+
 for n, line in enumerate(data, 1):
     cur_entry = line.rstrip().split(',')
     # handle first line which is useless
@@ -176,9 +174,29 @@ for n, line in enumerate(data, 1):
             # Assign tuple to location
             GYlist[gy_x][gy_y].append(cur_tuple)
 
+f.close()
+
+def regression (X, y): # return b_list, mean of err_list
+    n = len(X)
+    # (1) X'y Matrix
+    Xt = np.transpose (X)
+    X_y = np.dot (Xt, y)
+    # (2) X'X Matrix
+    Xt_X = np.dot (Xt, X)
+    # (6) Inverse Matrix
+    inv_Xt_X = np.linalg.inv(Xt_X)
+    b_list = np.dot(inv_Xt_X, X_y)
+    err_list = []
+    for i in range(n):
+        err_list.append(y[i] - np.dot (b_list,X[i]))
+    return [b_list, np.mean(err_list)]
+
 for i in range(num_genre+1):
     for j in range(9):
         target_list = GYlist[i][j]
+        if target_list == []:
+            model[i][j] = [ np.array([0.0 ,  0.0,  0.0]), 0.0]
+            continue
         user_list = []
         rating_list = []
         count_gender = count_age = count_occupation = 0
@@ -222,20 +240,55 @@ for i in range(num_genre+1):
         user_list = np.array(user_list)
         rating_list = np.array(rating_list)
         model[i][j] = regression (user_list, rating_list)
-print model
 
-#
-# def get_expected_rating(user, movie):
-# 	return 1
-#
-# file_test = open("test.txt","r")
-# line_test = []
-# for line in file_test:
-# 	line_test.append(line)
-#
-# for i in range(1, len(line_test)):
-# 	parse_line = line_test[i].split(",")
-# 	rating = get_expected_rating(parse_line[1],parse_line[2])
-# 	j = str(parse_line[0]) + " "
-# 	j += str(rating)
-# 	print(j)
+def calculate_rating (b_list, err, x_list):
+    return np.dot(b_list, x_list) + err
+
+def get_expected_rating(user, movie):
+    genre_list = dict_movie[movie].genre
+    gy_x_list = []
+    if genre_list == None:
+        gy_x_list.append(num_genre)
+    else:
+        for g in genre_list:
+            gy_x_list.append(genre_dict[g])
+    year = dict_movie[movie].year
+    if year == None:
+        gy_y = 8
+    else:
+        gy_y = min_max(year_min, year_max, year)
+    rating_list = []
+    for gy_x in gy_x_list:
+        user = user_dictionary[user]
+        if user.gender == None:
+            gender = user.gender_total*1.0/user.gender_number
+        else:
+            gender = int(user.gender)
+        if user.age == None:
+            age = user.age_total*1.0/user.age_number
+        else:
+            age = int(user.age)
+        if user.occupation == None:
+            occupation = user.occupation_total*1.0/user.occupation_number
+        else:
+            occupation = int(user.occupation)
+        x_list = np.array([gender,age,occupation])
+        rate = calculate_rating (model[gy_x][gy_y][0],model[gy_x][gy_y][1],x_list)
+        rating_list.append(rate)
+	return np.mean(rating_list)
+
+# Predict
+
+file_test = open("test.txt","r")
+line_test = []
+for line in file_test:
+	line_test.append(line)
+
+for i in range(1, len(line_test)):
+    parse_line = (line_test[i].strip()).split(",")
+    rating = int(get_expected_rating(parse_line[1],parse_line[2]))
+    rating = max(rating,1)
+    rating = min(rating,5)
+    j = str(parse_line[0]) + ","
+    j += str(rating)
+    print(j)
