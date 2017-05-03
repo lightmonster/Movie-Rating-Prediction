b94c3b6a7a792d1ac4792db78b814970da6bcd57
Suyie <qilezhi2@illinois.edu>
2017-05-03 09:42:58 -0500

    change y into labels 其实没有什么关系

--- RatingPredictor.py
+++ RatingPredictor.py
@@ -1,10 +1,11 @@
+from __future__ import print_function
 from __future__ import division
 import numpy as np
 import pandas as pd
 import nb_manual as nb
 
 # Read data from txt files
-print "Read data"
+print ("Read data")
 df_user = pd.read_csv('user.txt')
 df_movie = pd.read_csv('movie.txt')
 df_train = pd.read_csv('train.txt')
@@ -47,6 +48,27 @@ for e in ohe_element:
     df_train = df_train.drop([e], axis=1)
     df_train = pd.concat((df_train, dummy_table), axis=1)
 
+# genre_list = []
+# for genre in df_movie['Genre'].values:
+#     if type(genre) == str:
+#         genre = genre.split("|")
+#         for g in genre:
+#             if g not in genre_list:
+#                 genre_list.append(g)
+# genre_dict = dict()
+# for i in range(len(genre_list)):
+#     genre_dict[genre_list[i]] = i
+# genre_table = np.zeros((df_train.shape[0], len(genre_list)))
+# movie_value = df_train['Genre'].values  # the attr that we want to predict
+# for i in range(len(movie_value)):
+#     if type(movie_value[i]) == str:
+#         genre = movie_value[i].split("|")
+#         for g in genre:
+#             genre_table[i][genre_dict[g]] = 1
+#
+# df_train = df_train.drop(['Genre'], axis=1)
+# df_train = pd.concat((df_train, pd.DataFrame(genre_table)), axis=1)
+
 # Drop first two columns for predicting
 df_train.drop(['user-Id', 'movie-Id'], axis=1)
 
@@ -58,13 +80,12 @@ df_train.drop(['user-Id', 'movie-Id'], axis=1)
 v = df_train.values
 X = v[:piv_train]
 X_test = v[piv_train:]
-Y = labels-1
 
-print "BernoulliNB"
+print ("BernoulliNB")
 model = nb.BernoulliNB()  # Highest score
-print "fit"
-model.fit(X, Y)
-print "predict"
+print ("fit")
+model.fit(X, labels)
+print ("predict")
 predict_prob = model.predict_log_proba(X_test)
 
 ids, cts, cts1 = [], [], []
@@ -72,8 +93,8 @@ for i in range(len(id_test)):
     idx = id_test[i]
     ids += [idx]
     cts += (np.argsort(predict_prob[i])[::-1] + 1)[:1].tolist()
-print cts
+# print (cts)
 
 # generate submission file
 sub = pd.DataFrame(np.column_stack((ids, cts)), columns=['id', 'rating'])
-sub.to_csv('sub.csv', index=False)
+sub.to_csv('sub_ylabel.csv', index=False)
